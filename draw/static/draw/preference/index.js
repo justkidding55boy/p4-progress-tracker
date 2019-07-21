9e6e683253e77d43005c59b8cc9ae261a290d5e2
eijikudo <eijikudo@berkeley.edu>
2019-07-21 17:33:54 -0400

    1c

--- draw/static/draw/preference/index.js
+++ draw/static/draw/preference/index.js
@@ -5,8 +5,19 @@
                   'skyblue', 'navy', 'lime', 'aqua', 'fuchsia', 'olive'
                  , 'maroon'];
 
-    var user_history = [];
-    var user_id = Math.floor(Math.random() * colors.length);
+    var getColor = function() {
+      var i = 0;
+      do {
+
+        user_id = Math.floor(Math.random() * colors.length);
+        console.log(i);
+        i++;
+        //user_historyに見つけられなくなるまで続ける
+        //user_historyになかったら終わり
+      } while( i<20 || user_history.indexOf(user_id) !== -1);
+    }
+    
+    getColor();
   
      tool.onMouseDown = function(event) {
           if (path) {
@@ -25,7 +36,7 @@
      
       tool.onMouseDrag = function(event) {
         path.add(event.point);
-                var segment_list = [];
+        var segment_list = [];
         for (var i = 0; i < path.segments.length; i++) {
           segment_list.push(path.segments[i].point.x);
           segment_list.push(path.segments[i].point.y);
@@ -52,6 +63,7 @@
 //          socket.send(segment_list);
       }
       
+
   
     
     
--- draw/static/draw/preference/receive.js
+++ draw/static/draw/preference/receive.js
@@ -1,6 +1,21 @@
 var user_color;
       // getting the URL (you may want to use for Exercise 3)
     var url = window.location.href;
+    var url_large = false;
+    var url_small = false;
+    var url_normal = false;
+
+			if (url.indexOf("large") > -1) {
+				url_large = true;
+        $('.display_size').text('large');
+			} else if (url.indexOf("small") > -1){
+				url_small = true;
+        $('.display_size').text('small');
+			} else {
+        url_normal = true;
+        $('.display_size').text('normal');
+      }
+
 
     var socket = new WebSocket(
         'wss://p3-websockets-justkidding55boy-eijikudo883404.codeanyapp.com/ws/draw');
@@ -14,7 +29,7 @@ var user_color;
   //////////
   //////////
   socket.onmessage = function(msg) {
-    console.log(msg);
+    
       
       var segmentList = msg.data.split(",");
       
@@ -23,7 +38,19 @@ var user_color;
       for(var i = 0; i < segmentList.length-1; i+=3) {
         var x = parseInt(segmentList[i], 10);
         var y = parseInt(segmentList[i+1], 10);
-        tmp.strokeColor = colors[parseInt(segmentList[i+2], 10)];
+        var tmp_usrID = parseInt(segmentList[i+2], 10);
+        tmp.strokeColor = colors[tmp_usrID];
+        
+        if (user_history.indexOf(tmp_usrID) === -1)  {
+          user_history.push(tmp_usrID);
+        }
+       
+        
+       if (url_small) {
+         console.log("This is small");
+          return 
+       }
+        
         var p = new paper.Point(x, y);
         tmp.add(p);
       }
