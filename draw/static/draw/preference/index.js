3c249622ec7434664d243fe7fcc267699f7b89ff
eijikudo <eijikudo@berkeley.edu>
2019-08-11 18:48:12 -0400

    mid for erase

--- draw/static/draw/preference/index.js
+++ draw/static/draw/preference/index.js
@@ -7,40 +7,44 @@ paper.setup(canvas);
 
 var draw = function(){
   if (pen_check == 1) {
-    pen();
+    console.log("pen");
+    pen(cxt.globalCompositeOperation);
   } else if (path_check == 1) {
+    console.log("path");
     path_c();
-  } else if (erase_check == 0) {
+  } else if (erase_check == 1) {
+    console.log("erase");
     erase();
   }
 }
 
-function pen() {
-    var _x;
-    var _y;
-    var paintstat = false;
+function pen(type) {
+  console.log(type);
+  
+  var _x;
+  var _y;
+  var paintstat = false;
 
-    var tool = new paper.Tool();
-    tool.onMouseDown = function(event) {
-      paintstat = true;
-      _x = event.point.x
-      _y = event.point.y;
-    }
+  var tool = new paper.Tool();
+  tool.onMouseDown = function(event) {
+    paintstat = true;
+    _x = event.point.x
+    _y = event.point.y;
+  }
 
-    tool.onMouseMove = function(event) {
-      if (paintstat) {
-        cxt.beginPath();
-        //cxt.setLineDash([5, 15]);
-        point = event.point;
-        cxt.strokeStyle = "#000000";
-        cxt.moveTo(_x, _y);
-        cxt.lineTo(point.x, point.y);
-        cxt.lineWidth = 2;
-        cxt.stroke();
-        _x = point.x;
-        _y = point.y;
-      }
+  tool.onMouseMove = function(event) {
+    if (paintstat) {
+      cxt.beginPath();
+      //cxt.setLineDash([5, 15]);
+      point = event.point;
+      cxt.strokeStyle = "#000000";
+      cxt.moveTo(_x, _y);
+      cxt.lineTo(point.x, point.y);
+      cxt.stroke();
+      _x = point.x;
+      _y = point.y;
     }
+  }
 
     tool.onMouseUp = function(event) {
       paintstat = false;
@@ -129,18 +133,23 @@ function path_c() {
 $('#pen').click(function(){
   pen_check = 1;
   path_check = 0;
+  cxt.linewidth = 2;
   cxt.globalCompositeOperation = 'source-over';
   draw();
 });
 
 $('#path').click(function(){
-  pen_check = 1;
-  path_check = 0;
+  pen_check = 0;
+  path_check = 1;
   draw();
 });
 
 $('#erase').click(function(){
   pen_check = 0;
   path_check = 0;
-  cxt.globalCompositeOperation = "destination-out"
+  erase_check = 1;
+  cxt.linewidth = 10;
+  cxt.globalCompositeOperation = "destination-out";
+  cxt.linewidth = 10;
+  draw();
 });
