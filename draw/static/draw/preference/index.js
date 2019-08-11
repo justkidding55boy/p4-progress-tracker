f9225965497442986a0825a67f25fbd08e122889
eijikudo <eijikudo@berkeley.edu>
2019-08-11 19:24:53 -0400

    erase made

--- draw/static/draw/preference/index.js
+++ draw/static/draw/preference/index.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById('myCanvas');
 var cxt = canvas.getContext('2d');
-var pen_check = 1;
+var pen_check = 0;
 var path_check = 0;
 var erase_check = 0;
 paper.setup(canvas);
@@ -14,7 +14,6 @@ var draw = function(){
     path_c();
   } else if (erase_check == 1) {
     console.log("erase");
-    erase();
   }
 }
 
@@ -55,41 +54,6 @@ function pen(type) {
 /*function eraser_c(){
   
 }*/
-
-var erase = function(){
-  pen_check = 0;
-  path_check = 0;
-  //test
-//   var _x;
-//   var _y;
-//   var paintstat = false;
-//   var tool = new paper.Tool();
-//   tool.onMouseDown = function(event) {
-//     paintstat = true;
-//     _x = event.point.x;
-//     _y = event.point.y;
-//     cxt.save();
-    cxt.globalCompositeOperation = 'destination-out';
-//     cxt.strokeStyle = "red";
-//     cxt.moveTo(_x, _y);
-//     cxt.lineTo(_x, _y);
-//     cxt.lineWidth = 2;
-//     cxt.stroke();
-//   }
-  
-//   tool.onMouseDrag = function(event) {
-//     paintstat = true;
-//     _x = event.point.x;
-//     _y = event.point.y;
-//     cxt.save();
-//     cxt.globalCompositeOperation = 'destination-out';
-//     cxt.strokeStyle = "red";
-//     cxt.moveTo(_x, _y);
-//     cxt.lineTo(_x, _y);
-//     cxt.lineWidth = 2;
-//     cxt.stroke();
-//   }
-}
 
 function clear_c() {
   cxt.clearRect(0, 0, canvas.width, canvas.height);
@@ -133,8 +97,9 @@ function path_c() {
 $('#pen').click(function(){
   pen_check = 1;
   path_check = 0;
-  cxt.linewidth = 2;
+//   cxt.linewidth = 30;
   cxt.globalCompositeOperation = 'source-over';
+  cxt.lineWidth = 2;
   draw();
 });
 
@@ -145,11 +110,12 @@ $('#path').click(function(){
 });
 
 $('#erase').click(function(){
+  
   pen_check = 0;
   path_check = 0;
   erase_check = 1;
-  cxt.linewidth = 10;
+  cxt.lineWidth = 30;
   cxt.globalCompositeOperation = "destination-out";
-  cxt.linewidth = 10;
+
   draw();
 });
