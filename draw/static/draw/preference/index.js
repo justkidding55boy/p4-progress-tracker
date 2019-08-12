a385fca2675924c44ca8e58c1c18dca3cd38419d
eijikudo <eijikudo@berkeley.edu>
2019-08-12 01:42:31 -0400

    implementation of erase && fix the bag

--- draw/static/draw/preference/index.js
+++ draw/static/draw/preference/index.js
@@ -3,40 +3,62 @@ var cxt = canvas.getContext('2d');
 var pen_check = 0;
 var path_check = 0;
 var erase_check = 0;
+
 paper.setup(canvas);
+var paths = [];
+var paths_redo = [];
 
 var draw = function(){
-  if (pen_check == 1) {
-    console.log("pen");
-    pen(cxt.globalCompositeOperation);
-  } else if (path_check == 1) {
-    console.log("path");
-    path_c();
-  } else if (erase_check == 1) {
-    console.log("erase");
-  }
-}
-
-function pen(type) {
-  console.log(type);
-  
+  var tool = new paper.Tool();
+  var myPath;
+  //used for eraser
   var _x;
   var _y;
-  var paintstat = false;
-
-  var tool = new paper.Tool();
+  var  paintstat = false; 
+ ////
+  
   tool.onMouseDown = function(event) {
-    paintstat = true;
-    _x = event.point.x
-    _y = event.point.y;
+    if (pen_check == 0 && path_check == 1 && erase_check == 0) {
+      //path
+      console.log("path function");
+      myPath = new paper.Path({
+      strokeColor: 'red',
+      strokeWidth: 5,
+      strokeCap: 'round',
+      dashArray: [10, 10]
+    });
+
+    } else if (pen_check == 1 && path_check == 0 && erase_check == 0) {
+      //pen
+      console.log("pen function");
+      myPath = new paper.Path({
+        strokeColor: 'black',
+        strokeWidth: 2
+      });
+      
+      //erase
+    } else if (pen_check == 0 && path_check == 0 && erase_check == 1) {
+      //erase 
+      paintstat = true;
+      _x = event.point.x;
+      _y = event.point.y;
+    }
+     
+
   }
 
-  tool.onMouseMove = function(event) {
-    if (paintstat) {
+  tool.onMouseDrag = function(event) {
+    
+    if (pen_check == 0 && path_check == 1 && erase_check == 0) {
+      //path
+      myPath.add(event.point); 
+    } else if (pen_check == 1 && path_check == 0 && erase_check == 0) {
+      //pen
+      myPath.add(event.point);
+    } else if (pen_check == 0 && path_check == 0 && erase_check == 1) {
+      //erase
       cxt.beginPath();
-      //cxt.setLineDash([5, 15]);
       point = event.point;
-      cxt.strokeStyle = "#000000";
       cxt.moveTo(_x, _y);
       cxt.lineTo(point.x, point.y);
       cxt.stroke();
@@ -44,16 +66,20 @@ function pen(type) {
       _y = point.y;
     }
   }
-
-    tool.onMouseUp = function(event) {
+ 
+  tool.onMouseUp = function(event) {
+    //arrow
+    if (pen_check == 0 && path_check == 1 && erase_check == 0) {
+      console.log(myPath);
+      paths.push(myPath);
+    } else if (pen_check == 1 && path_check == 0 && erase_check == 0) {
       paintstat = false;
+    } else if (pen_check == 0 && path_check == 0 && erase_check == 1) {
+      
     }
 
+  }
 }
-
-/*function eraser_c(){
-  
-}*/
 
 function clear_c() {
   cxt.clearRect(0, 0, canvas.width, canvas.height);
@@ -62,60 +88,55 @@ function clear_c() {
   path_check = 0;
 }
 
-
-function path_c() {
-  var _x;
-  var _y;
-  var paintstat = false;
-  var tool = new paper.Tool();
-  tool.onMouseDown = function(event) {
-    paintstat = true;
-    _x = event.point.x
-    _y = event.point.y;
-  }
-
-  tool.onMouseMove = function(event) {
-    if (paintstat) {
-      cxt.beginPath();
-      cxt.setLineDash([5, 15]);
-      point = event.point;
-      cxt.strokeStyle = "#000000";
-      cxt.moveTo(_x, _y);
-      cxt.lineTo(point.x, point.y);
-      cxt.lineWidth = 2;
-      cxt.stroke();
-      _x = point.x;
-      _y = point.y;
-    }
-  }
-
-  tool.onMouseUp = function(event) {
-    paintstat = false;
-  }
-}
-
 $('#pen').click(function(){
+  console.log('pen clicked');
   pen_check = 1;
   path_check = 0;
+  erase_check = 0;
 //   cxt.linewidth = 30;
   cxt.globalCompositeOperation = 'source-over';
   cxt.lineWidth = 2;
   draw();
+  
 });
 
-$('#path').click(function(){
+$('#path').click(function() {
+  console.log("path clicked");
   pen_check = 0;
   path_check = 1;
+  erase_check = 0;
+  cxt.globalCompositeOperation = 'source-over';
   draw();
 });
 
-$('#erase').click(function(){
-  
+$('#erase').click(function() {
   pen_check = 0;
   path_check = 0;
   erase_check = 1;
   cxt.lineWidth = 30;
   cxt.globalCompositeOperation = "destination-out";
-
+  
   draw();
+});
+
+$('.undo-icon').click(function() {
+  console.log(paths);
+  var target = paths[paths.length-1];
+  target.removeSegments();
+//   console.log("popinfo" +  paths.pop());
+  paths_redo.push(target);
+  paths.pop();
+});
+
+$('.redo-icon').click(function(){
+  console.log("redo-click");
+  var myPath = new paper.Path({
+    strokeColor: 'red'
+  });
+  
+  var target = paths_redo[paths_redo.length-1];
+
+  paths_redo.pop();
+  
+
 });
